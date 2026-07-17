68aaf11b5c33e39c230cd19492c3be009899efdf
Your Name <you@example.com>
2026-07-17 21:36:26 +0000

    lol

--- configuration.nix
+++ configuration.nix
@@ -19,8 +19,10 @@
   services.xserver.enable = true;
   # services.xserver.displayManager.lightdm.enable = true;
   # services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
   # services.displayManager.defaultSession = "xfce";
   services.displayManager.defaultSession = "gnome";
 
@@ -43,11 +45,11 @@
 
   environment.systemPackages = with pkgs; [
     git
-    nvim
+    neovim
     nodejs_22
-    # fzf
-    # rg
-    # fd
+    fzf
+    ripgrep
+    fd
   ];
 
   nix.settings.experimental-features = [
